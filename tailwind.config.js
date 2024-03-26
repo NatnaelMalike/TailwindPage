0f1bd2d7ad337c913e3e66e9b0c9f4c26c1ff946
natnael <natnaelmalike@gmail.com>
2024-03-26 20:44:58 +0300

    finalizing the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./built/*.html","./built/js/*.js"],
+  content: ["./built/**/*.{html,js}"],
   theme: {
     extend: {
       screens: {
         widescreen: { raw: "(min-aspect-ratio: 3/2)" },
-        tallscreen: { raw: "(min-aspect-ratio: 13/20)" },
+        tallscreen: { raw: "(max-aspect-ratio: 13/20)" },
       },
       keyframes: {
         'open-menu':{
